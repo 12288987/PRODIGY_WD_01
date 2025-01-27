9616cc54606456d6fbc2ee731a8727fd15dedc86
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-27 21:35:47 +0300

    scroll event added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,42 @@
 const header = document.getElementById("header");
-header.addEventListener('mouseover', function() {
+const list1 = document.getElementById("1");
+const list2 = document.getElementById("2");
+const list3 = document.getElementById("3");
+
+// about
+list1.addEventListener('mouseover', function() {
     document.getElementById("nav").style.backgroundColor = "red";
     document.getElementById("1").style.color = "blue";
-    document.getElementById("2").style.color = "blue";
-    document.getElementById("3").style.color = "blue";
 })
-header.addEventListener('pointerleave', function() {
+list1.addEventListener('pointerleave', function() {
     document.getElementById("nav").style.backgroundColor = "black";
     document.getElementById("1").style.color = "white";
+})
+
+// resume
+list2.addEventListener('mouseover', function() {
+    document.getElementById("nav").style.backgroundColor = "red";
+    document.getElementById("2").style.color = "blue";
+})
+list2.addEventListener('pointerleave', function() {
+    document.getElementById("nav").style.backgroundColor = "black";
     document.getElementById("2").style.color = "white";
+})
+
+// projects
+list3.addEventListener('mouseover', function() {
+    document.getElementById("nav").style.backgroundColor = "red";
+    document.getElementById("3").style.color = "blue";
+})
+list3.addEventListener('pointerleave', function() {
+    document.getElementById("nav").style.backgroundColor = "black";
     document.getElementById("3").style.color = "white";
+})
+
+document.addEventListener('scroll', (event) => {
+    document.getElementById("nav").style.backgroundColor = "red"
+})
+
+document.addEventListener('scrollend', (event) => {
+    document.getElementById("nav").style.backgroundColor = "black"
 })
